a3231d52e6716c1af04de45660acf12b218f1b1b
David Denton <denton.david@gmail.com>
2017-09-27 16:49:23 +0100

    Adding previous steps

--- step1/src/main/scala/security.scala
+++ step1/src/main/scala/security.scala
@@ -1,6 +1,11 @@
 /**
   * 1. Implement a async Service, DirectoryService, using the core Finagle API, which takes an Int and looks up a corresponding name.
   * If there is no name found, throw an UnknownId
+  *
+  * The known users are:
+  * id 1 -> Rita
+  * id 2 -> Sue
+  * id 3 -> Bob
   */
 
 case class UnknownId(id: Int) extends Exception(s"the user $id is unknown")
