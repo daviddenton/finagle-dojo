9e597e91f2ed90beef931c88f96704951d5337a5
David Denton <denton.david@gmail.com>
2017-09-28 14:18:00 +0100

    more work on steps

--- step8/src/main/scala/dojo/SecurityServer.scala
+++ step8/src/main/scala/dojo/SecurityServer.scala
@@ -3,11 +3,12 @@ package dojo
 import com.twitter.finagle.{Http, ListeningServer}
 
 class SecurityServer(port: Int) {
-  def start(): ListeningServer = {
-    val service = new ConvertMessage().andThen(
-      new SecurityCheck(new UserDirectory())
-    )
 
-    Http.serve("localhost:8000", service)
+  def start(): ListeningServer = {
+    val httpSecurityCheck = new Audit()
+      .andThen(new ConvertMessage())
+      .andThen(new SecurityCheck(new UserDirectory()))
+
+    Http.serve("localhost:8000", httpSecurityCheck)
   }
 }
